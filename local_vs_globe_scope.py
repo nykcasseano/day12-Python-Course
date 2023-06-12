bca9c161745582d5763a396d312d038764f8c774
nyk <nykcasseano13@gmail.com>
2023-06-12 15:33:53 -0300

    último commit

--- local_vs_globe_scope.py
+++ local_vs_globe_scope.py
@@ -21,3 +21,12 @@ print(potion_strength)
 
 
 #Global Scope
+
+player_health = 10
+
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+    
+drink_potion()
+print(potion_strength)
